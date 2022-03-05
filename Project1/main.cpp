c3438b918f79a45398279298908eafc9d84feb46
chloebeth <chloe.e.muncy@gmail.com>
2022-03-04 21:34:48 -0500

    boost and signing

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
-#include "CoreLibrary.h"
+#include "..\CoreLibrary\CoreLibrary.h"
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <boost/locale.hpp>
 
+
+using namespace std;
 
 int main(){
 	GetCoreLibrary()->InitializingApplication();
-	std::cout << "hello world" << std::endl;
+
+	boost::locale::generator gen;
+	gen.add_messages_path("C:\\Users\\Chloe\\Documents\\Soft Arch\\software-architecture-midterm\\Project1");
+	gen.add_messages_domain("hello");
+
+	locale::global(gen("de_DE.UTF - 8")); 
+	//locale::global(gen(""));
+	cout.imbue(locale());
+
+	cout << boost::locale::translate("hello world") << endl;
+
+	cout << boost::locale::translate("file", "open") << endl;
+	cout << boost::locale::translate("file", "close") << endl;
+
 	return 0;
 }
