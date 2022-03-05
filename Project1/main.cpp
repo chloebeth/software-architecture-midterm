15cec983382556e681f96bc9a60b4157916cd99e
chloebeth <chloe.e.muncy@gmail.com>
2022-03-04 21:52:24 -0500

    german translation with boost

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -12,17 +12,14 @@ int main(){
 	GetCoreLibrary()->InitializingApplication();
 
 	boost::locale::generator gen;
-	gen.add_messages_path("C:\\Users\\Chloe\\Documents\\Soft Arch\\software-architecture-midterm\\Project1");
-	gen.add_messages_domain("hello");
+	gen.add_messages_path(".");
+	gen.add_messages_domain("messages");
 
-	locale::global(gen("de_DE.UTF - 8")); 
+	locale::global(gen("de_DE.UTF-8")); 
 	//locale::global(gen(""));
 	cout.imbue(locale());
 
 	cout << boost::locale::translate("hello world") << endl;
-
-	cout << boost::locale::translate("file", "open") << endl;
-	cout << boost::locale::translate("file", "close") << endl;
 
 	return 0;
 }
